fb39c76fcbf46c77112cc92b025b4eb0aec454b9
miya0001 <miya@wpist.me>
2016-04-03 11:17:44 +0900

    room feature

--- app.js
+++ app.js
@@ -3,17 +3,29 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
+var redis = require('socket.io-redis');
+io.adapter(redis({ host: 'localhost', port: 6379 }));
+io.sockets.setMaxListeners(0);
 
-server.listen(port, function () {
-  console.log('Server listening at port %d', port);
-});
+var port = process.env.PORT || 3000;
+server.listen(port);
+
+var get_room = function(socket) {
+  return socket.request._query.app;
+}
 
 // Routing
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function (socket) {
-  socket.on('push', function (data) {
-    socket.broadcast.emit('push', data);
-  });
+  var room = get_room(socket);
+  if (room) {
+    socket.join(room);
+    io.to(room).emit('join', room);
+    socket.on('push', function (data) {
+      io.to(room).emit('push', data);
+    });
+  } else {
+    io.sockets.sockets[socket.id].disconnect();
+  }
 });
--- tests/test.js
+++ tests/test.js
@@ -2,18 +2,26 @@ var should = require('should');
 var io     = require('socket.io-client');
 var server = require('../app');
 
-var socketURL = 'http://localhost:3000';
-
-var options ={
-  transports: ['websocket'],
-  'force new connection': true
-};
-
 describe("Simple Chat",function(){
 
   it('Should get username and message',function(done){
     // Send a message
-    var client1 = io.connect(socketURL, options);
+    var client1 = io.connect('http://localhost:3000', {
+      transports: ['websocket'],
+      'force new connection': true
+    });
+    client1.on('connect', function(){
+      done();
+    });
+  });
+
+  it('Should get username and message',function(done){
+    // Send a message
+    var client1 = io.connect('http://localhost:3000', {
+      transports: ['websocket'],
+      'force new connection': true,
+      query: "app=1234"
+    });
     client1.on('connect', function(){
       client1.emit('push', {
         username: 'John',
@@ -22,8 +30,15 @@ describe("Simple Chat",function(){
     });
 
     // Get a message
-    var client2 = io.connect(socketURL, options);
+    var client2 = io.connect('http://localhost:3000', {
+      transports: ['websocket'],
+      'force new connection': true,
+      query: "app=1234"
+    });
     client2.on('connect', function(){
+      client2.on('join', function(data){
+        data.should.equal('1234');
+      });
       client2.on('push', function(data){
         data.should.have.property('username', 'John');
         data.should.have.property('message', 'Hello World');
@@ -32,4 +47,8 @@ describe("Simple Chat",function(){
       });
     });
   });
+
+  // it('Another room can not get data',function(done){
+  //
+  // });
 });
